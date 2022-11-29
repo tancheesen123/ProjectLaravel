335ddac73f566b619c4bce0517971228f4d1a016
tancheesen123 <78629638+tancheesen123@users.noreply.github.com>
2022-11-29 16:23:30 +0800

    Multi Authentication Admin & User Login System

--- laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('usertype')->default(0);
             $table->string('phone')->nullable();
             $table->string('address')->nullable();
             $table->timestamp('email_verified_at')->nullable();
